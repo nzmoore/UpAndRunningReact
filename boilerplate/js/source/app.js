a18570fd670232e2762e39a86edf05cae4d168af
Bill Moore <bill@nzmoore.com>
2017-06-29 23:46:59 -0400

    Added application boilerplate example

---
+++ boilerplate/js/source/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo'
+ReactDOM.render(
+  <h1>
+    <Logo /> Welcome to the app!
+  </h1>,
+  document.getElementById('app')
+);
