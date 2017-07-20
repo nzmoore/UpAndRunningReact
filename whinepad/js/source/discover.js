c83c6c4d4b035dc8accdc16bcd7a0fb640754f8a
Bill Moore <bill@nzmoore.com>
2017-07-20 17:15:04 -0400

    Creating Whinepad - Component dicovery and Button component done

---
+++ whinepad/js/source/discover.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import Logo from './components/Logo';
+import Button from './components/Button';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div style ={ { padding: '20px' } }>
+    <h1>Component discovery</h1>
+
+    <h2>Logo</h2>
+    <div style= { { display: 'inline-block', background: 'purple'} } >
+      <Logo />
+    </div>
+
+    <h2>Buttons</h2>
+    <div>Button with onClick: <Button onClick={ () => alert('Ouch')}>Click Me</Button></div>
+    <div>A link: <Button href="http://reactjs.com">Follow me</Button></div>
+    <div>Custom class name: <Button className="custom">I do nothing</Button></div>
+
+  </div>,
+  document.getElementById('pad')
+);
